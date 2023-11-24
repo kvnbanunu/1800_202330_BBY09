d78fa6733ca677839e37918e9444bf7c7abdb4bc
Kevin Nguyen <kevinbanunu@outlook.com>
2023-11-24 15:32:40 -0800

    Renamed the old files with old

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,37 @@
+let fbAuth = firebase.auth();
+
 function getNameFromAuth() {
-  firebase.auth().onAuthStateChanged(user => {
-    // Check if a user is signed in:
+  fbAuth.onAuthStateChanged(user => {
+    // Check if user is signed in:
     if (user) {
-      // Do something for the curretnly logged-in user here:
-      console.log(user.uid); //print the uid in the browser console
-      console.log(user.displayName); //print the user name in the browser console
-      userName = user.displayName;
-
-      document.getElementById("name-goes-here").innerText = userName;
-
+      //Do something for the currently logged-in user here:
+      fbAuth.onAuthStateChanged(user => {
+        let userID = db.collection("users").doc(user.uid);
+        userID.get()
+        .then(userDetails => {
+          userName = userDetails.data().displayName;
+          document.getElementById("name-goes-here").innerText = userName;
+        })
+      })
     } else {
       // No user is signed in.
     }
-  });
+  })
 }
-getNameFromAuth(); //run the function
+getNameFromAuth();
 
 function populateCurrentPoints() {
-  firebase.auth().onAuthStateChanged(user => {
-    var currentPoints = db.collection("users").doc(user.uid);
-    currentPoints.get()
-      .then(userDoc => {
-        var cp = userDoc.data().points;
-        document.getElementById("currentPoints").value = cp;
-      })
+  fbAuth.onAuthStateChanged(user => {
+    let userID = db.collection("users").doc(user.uid);
+    userID.get()
+    .then(userDoc => {
+      cp = userDoc.data().points;
+      document.getElementById("currentPoints").value = cp;
+    })
   })
-  
 }
 populateCurrentPoints();
 
-
-// Haversine formula to find the distance(km) between two sets of latitudes and longitudes.
 function distance(lat1, lon1, lat2, lon2) {
   const r = 6371; // km
   const p = Math.PI / 180;
@@ -42,78 +43,93 @@ function distance(lat1, lon1, lat2, lon2) {
   return 2 * r * Math.asin(Math.sqrt(a));
 }
 
-function populateLocation() {
-
-  var stop1 = db.collection("stopNo").doc(document.getElementById("stopNo1").value)
-  stop1.get()
-    .then(stopDoc => {
-      var lat1 = stopDoc.data().latitude;
-      var lon1 = stopDoc.data().longitude;
-      document.getElementById("lat1").value = lat1;
-      document.getElementById("lon1").value = lon1;
-    })
-
-  var stop2 = db.collection("stopNo").doc(document.getElementById("stopNo2").value)
-  stop2.get()
-    .then(stopDoc => {
-      var lat2 = stopDoc.data().latitude;
-      var lon2 = stopDoc.data().longitude;
-      document.getElementById("lat2").value = lat2;
-      document.getElementById("lon2").value = lon2;
-    })
-}
-
-function gainPoints() {
-  
-  var km = distance(
-    document.getElementById("lat1").value, 
-    document.getElementById("lon1").value, 
-    document.getElementById("lat2").value, 
-    document.getElementById("lon2").value
-    );
-  
-  var pointsEarned = Math.round(km * 10);
-  console.log("Points Earned: " + pointsEarned);
-  document.getElementById("points").value = pointsEarned;
-
-  firebase.auth().onAuthStateChanged(user => {
-    var userPoints = db.collection("users").doc(user.uid);
-    userPoints.get()
-      .then(pointsDoc => {
-        var treeLvl = pointsDoc.data().treeLevel;
-        var nextTree = 100 * treeLvl
-        var treesPlanted = pointsDoc.data().treesPlanted;
-        var currentPoints = pointsDoc.data().points;
-        var pointsTotal = currentPoints + pointsEarned;
-        if (pointsTotal >= nextTree) {
-          ++treeLvl;
-          ++treesPlanted;
-          userPoints.update({
-            treeLevel: treeLvl,
-            treesPlanted: treesPlanted
-          })
-        }
-        userPoints.update({
-          points: pointsTotal
-        })
-        .then(populateCurrentPoints())
-        .then(populateTrees())
-      })
-  });
-
-}
-
+let treeLvl = 1;
+let treeLvlThreshold = 100;
+let nextTree = 100;
+let noTrees = 0;
 function populateTrees() {
-  firebase.auth().onAuthStateChanged(user => {
-    var userTrees = db.collection("users").doc(user.uid);
-    userTrees.get()
+  fbAuth.onAuthStateChanged(user => {
+    let userID = db.collection("users").doc(user.uid);
+    userID.get()
       .then(treeDoc => {
-        var treeLvl = 100 * treeDoc.data().treeLevel;
-        var nextTree = treeLvl - treeDoc.data().points;
-        var noTrees = treeDoc.data().treesPlanted;
+        treeLvl = treeDoc.data().treeLevel;
+        treeLvlThreshold = 100 * treeLvl;
+        nextTree = treeLvlThreshold - treeDoc.data().points;;
+        noTrees = treeDoc.data().treesPlanted;
         document.getElementById("nextTree").innerText = nextTree;
         document.getElementById("noTrees").innerText = noTrees;
       })
   })
 }
 populateTrees();
+
+let lat1 = 0;
+let lon1 = 0;
+let lat2 = 0;
+let lon2 = 0;
+async function getLocation() {
+  let stopNo1 = document.getElementById("stopNo1").value;
+  let url1 = 'https://api.translink.ca/rttiapi/v1/stops/' + stopNo1 + '?apikey=QFhRbhrC252PYLGdcdcn';
+  let stopNo2 = document.getElementById("stopNo2").value;
+  let url2 = 'https://api.translink.ca/rttiapi/v1/stops/' + stopNo2 + '?apikey=QFhRbhrC252PYLGdcdcn';
+  
+  try {
+    let response1 = await fetch(url1, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    let data1 = await response1.json();
+    lat1 = data1.Latitude;
+    lon1 = data1 .Longitude;
+
+    let response2 = await fetch(url2, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    let data2 = await response2.json();
+    lat2 = data2.Latitude;
+    lon2 = data2.Longitude;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+let pointsTotal = 0;
+async function gainPoints() {
+  await getLocation();
+  console.log("lat1: " + lat1);
+  console.log("lon1: " + lon1);
+  console.log("lat2: " + lat2);
+  console.log("lon2: " + lon2);
+  let km = distance(lat1, lon1, lat2, lon2);
+  console.log(km);
+  let pointsEarned = Math.floor(km * 10);
+  if (pointsEarned < 1) {
+    pointsEarned = 1;
+  } else {};
+  console.log("Points Earned: " + pointsEarned);
+  document.getElementById("points").value = pointsEarned;
+
+  fbAuth.onAuthStateChanged(user => {
+    let userID = db.collection("users").doc(user.uid);
+    userID.get()
+      .then(pointsDoc => {
+        pointsTotal = cp + pointsEarned;
+        if (pointsTotal >= treeLvlThreshold) {
+          ++treeLvl;
+          ++noTrees;
+          userID.update({
+            treeLevel: treeLvl,
+            treesPlanted: noTrees
+          })
+        } else {};
+        userID.update({
+          points: pointsTotal
+        })
+        .then(populateCurrentPoints())
+        .then(populateTrees());
+      })
+  });
+}
